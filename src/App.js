529afb110471eacf35934b2e6d48ea8693e1d5eb
SMarcoG <sara7915zuera@gmail.com>
2024-02-12 10:22:11 +0100

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import Player from './Player/Player'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 1. definir variables de estado usando useState (activePlayer, score, current, diceNumber)
+// 2. definir funciones para manejar los eventos de click (handleNewGame, handleRollDice, handleHold)
+// 3. pasar las variables de estado y las funciones a los componentes Player y Dice
+// 4. manejar los eventos de click en los botones de New game, Roll dice y Hold
+// 5. manejar el cambio de imagen de dado cuando se hace click en el botón rolldice
+// 5. manejar el cambio de jugador activo cuando se hace click en el botón Hold
+// 6. manejar el cambio de jugador activo cuando se obtiene un 1 al hacer click en el botón Roll dice
+// 7. manejar el cambio de jugador activo cuando se obtiene un 6 al hacer click en el botón Roll dice
+// 8. manejar el cambio de jugador activo cuando se obtiene un número diferente de 1 o 6 al hacer click en el botón Roll dice
+// 9. manejar el cambio de jugador activo cuando se hace click en el botón New game
+
+const handleRollDice = () => {
+  setDiceNumber(Math.floor(Math.random() * 6) + 1)
 }
 
-export default App;
+const handleNewGame = () => {
+  setActivePlayer(1)
+  setScore
+}
+
+useEffect(() => {
+  if (diceNumber === 1){
+      setActivePlayer((activePlayer) => (activePlayer === 1 ? 2 : 1))
+      setCurrent(0)
+  }else{
+      setCurrent((current) => current + diceNumber)
+  }
+}, [diceNumber])
+
+const handleHold = () => {
+  const newScore = [...score]
+  newScore = [activePlayer]
+}
+
+
+
+
+function App() {
+  const [activePlayer, setActivePlayer] = useState(2)
+  const [score, setScore] = useState([3, 6])
+  const [current, setCurrent] = useState(7)
+  const [diceNumber, setDiceNumber] = useState(3)
+
+  return (
+    <main>
+      <Player
+        name="Player 1"
+        score={score[0]}
+        current={activePlayer === 1 && current}
+        isActive={activePlayer === 1}
+      />
+      <Player
+        name="Player 2"
+        score={score[1]}
+        current={activePlayer === 2 && current}
+        isActive={activePlayer === 2}
+      />
+      {diceNumber && (
+        <img
+          src={`dice-${diceNumber}.png`}
+          alt="Playing dice"
+          className="dice"
+        />
+      )}
+      <button className="btn btn--new">:arrows_counterclockwise: New game</button>
+      <button className="btn btn--roll">:game_die: Roll dice</button>
+      <button className="btn btn--hold">:inbox_tray: Hold</button>
+    </main>
+  )
+}
+
+export default App
